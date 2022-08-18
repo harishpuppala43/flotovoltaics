c63adcdacf77295b6785a80e3774804fecc7f1a1
harishpuppala43 <harish.puppala@bmu.edu.in>
2022-08-18 14:59:47 +0530

    Add existing file

--- layers/layers.js
+++ layers/layers.js
@@ -26,29 +26,29 @@ var lyr_India_1 = new ol.layer.Vector({
                 interactive: true,
                 title: '<img src="styles/legend/India_1.png" /> India'
             });
-var format_reservoirs_2 = new ol.format.GeoJSON();
-var features_reservoirs_2 = format_reservoirs_2.readFeatures(json_reservoirs_2, 
+var format_Hydropowerreservoirs_2 = new ol.format.GeoJSON();
+var features_Hydropowerreservoirs_2 = format_Hydropowerreservoirs_2.readFeatures(json_Hydropowerreservoirs_2, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_reservoirs_2 = new ol.source.Vector({
+var jsonSource_Hydropowerreservoirs_2 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_reservoirs_2.addFeatures(features_reservoirs_2);
-var lyr_reservoirs_2 = new ol.layer.Vector({
+jsonSource_Hydropowerreservoirs_2.addFeatures(features_Hydropowerreservoirs_2);
+var lyr_Hydropowerreservoirs_2 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_reservoirs_2, 
-                style: style_reservoirs_2,
+                source:jsonSource_Hydropowerreservoirs_2, 
+                style: style_Hydropowerreservoirs_2,
                 interactive: true,
-                title: '<img src="styles/legend/reservoirs_2.png" /> reservoirs'
+                title: '<img src="styles/legend/Hydropowerreservoirs_2.png" /> Hydropower reservoirs'
             });
 
-lyr_OSMStandard_0.setVisible(true);lyr_India_1.setVisible(true);lyr_reservoirs_2.setVisible(true);
-var layersList = [lyr_OSMStandard_0,lyr_India_1,lyr_reservoirs_2];
+lyr_OSMStandard_0.setVisible(true);lyr_India_1.setVisible(true);lyr_Hydropowerreservoirs_2.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_India_1,lyr_Hydropowerreservoirs_2];
 lyr_India_1.set('fieldAliases', {'State': 'State', });
-lyr_reservoirs_2.set('fieldAliases', {'Latitude': 'Latitude', 'Longitude': 'Longitude', 'Reservoir': 'Reservoir', 'Area (Hec)': 'Area (Hec)', });
-lyr_India_1.set('fieldImages', {'State': '', });
-lyr_reservoirs_2.set('fieldImages', {'Latitude': 'TextEdit', 'Longitude': 'TextEdit', 'Reservoir': 'TextEdit', 'Area (Hec)': 'TextEdit', });
+lyr_Hydropowerreservoirs_2.set('fieldAliases', {'Latitude': 'Latitude', 'Longitude': 'Longitude', 'Reservoir': 'Reservoir', 'Area (Hec)': 'Area (Hec)', });
+lyr_India_1.set('fieldImages', {'State': 'TextEdit', });
+lyr_Hydropowerreservoirs_2.set('fieldImages', {'Latitude': '', 'Longitude': '', 'Reservoir': '', 'Area (Hec)': '', });
 lyr_India_1.set('fieldLabels', {'State': 'no label', });
-lyr_reservoirs_2.set('fieldLabels', {'Latitude': 'inline label', 'Longitude': 'inline label', 'Reservoir': 'inline label', 'Area (Hec)': 'inline label', });
-lyr_reservoirs_2.on('precompose', function(evt) {
+lyr_Hydropowerreservoirs_2.set('fieldLabels', {'Latitude': 'inline label', 'Longitude': 'inline label', 'Reservoir': 'inline label', 'Area (Hec)': 'inline label', });
+lyr_Hydropowerreservoirs_2.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- styles/Hydropowerreservoirs_2_style.js
+++ styles/Hydropowerreservoirs_2_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_reservoirs_2 = function(feature, resolution){
+var style_Hydropowerreservoirs_2 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
@@ -21,8 +21,8 @@ var style_reservoirs_2 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        image: new ol.style.Circle({radius: 3.999999999999997 + size,
-            stroke: new ol.style.Stroke({color: 'rgba(50,87,128,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 1}), fill: new ol.style.Fill({color: 'rgba(72,123,182,1.0)'})}),
+        image: new ol.style.Circle({radius: 4.0 + size,
+            stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0}), fill: new ol.style.Fill({color: 'rgba(133,182,111,1.0)'})}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
